4c100827b453af560a5e7d57cb6c13d9a46bfa92
bharathibk <bharathikalai.bk@gmail.com>
2024-03-24 19:17:24 +0530

    hangman project completed

--- hangman.py
+++ hangman.py
@@ -13,14 +13,62 @@ print("word",word)
 
 if __name__ == '__main__':
     print('Guess the word! HINT:word is a name of a fruit')
-
     for i in word:
         print('_', end=' ')
     print()
-
     playing = True
-
     lettergussed = ' '
-
     chances = len(word) + 2
-    print(chances)
+    correct = 0
+    flag = 0
+    try: 
+        while (chances !=0) and flag == 0:
+            print()
+            chances -=1
+            print("chances",chances)
+            try:
+                guess = str(input('enter a letter to guess: '))
+            except:
+                print('Enter only a Letter')
+                continue
+            if not guess.isalpha():
+                print('Enter only a Letter')
+                continue
+            elif len(guess)  > 1:
+                print('Enter only a Single Letter')
+                continue
+            elif guess in lettergussed:
+                print('you have already guessed that letter')
+                continue
+            if guess in word:
+                k = word.count(guess)
+                for _ in range(k):
+                    lettergussed += guess
+            for char in word:
+                if char in lettergussed and (Counter(lettergussed) != Counter(word)):
+                    print(char, end= '')
+                    correct +=1
+
+                elif (Counter(lettergussed) == Counter(word)):
+                    
+                    print("The word is: ", end= ' ')
+                    print(word)
+
+                    flag = 1
+
+                    print('congratulation ypou won')
+                    break
+                    break
+                else:
+                    print('__', end= '')
+
+        if chances  <=0 and (Counter(lettergussed) != Counter(word)):
+            print()
+            print('you lost try again')
+
+            print('the word was {}'.format(word))
+    except KeyboardInterrupt:
+        print()
+        print('bye try again')
+        exit()
+
