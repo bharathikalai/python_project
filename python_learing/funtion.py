b54aaf7975d10be8b8d065af61070ec7a88e7916
bharathibk <bharathikalai.bk@gmail.com>
2024-04-05 10:34:28 +0530

    new changes in function file

--- python_learing/funtion.py
+++ python_learing/funtion.py
@@ -37,3 +37,21 @@ def factorial(n):
 result = factorial(5)
 
 print("factorial of 5 is ",result)
+
+
+def factorial(n):
+    result = 1
+    for i in range(1, n +1):
+        result  *= i
+    return result
+
+result = factorial(100)
+
+print("factorial of 100 is ", result)
+
+
+result = 10
+for i in range(1,100):
+    result = result * i
+
+    print(result,"result")
