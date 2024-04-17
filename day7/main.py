96218e5a5a91bad886af2f295ce5486ded6fb33c
bharathibk <bharathikalai.bk@gmail.com>
2024-04-17 06:06:27 +0530

    day 7 leetcode 1st

--- day7/main.py
+++ day7/main.py
@@ -1,1 +1,20 @@
-#leet code practice
+#leet code practice problem 1
+def two_sum(nums,target):
+
+    num_indices = {}
+
+    for i,num in enumerate(nums):
+        complement = target - num
+
+        if complement in num_indices:
+            return [num_indices[complement],i]
+        num_indices[num] = i
+
+    return []
+
+
+
+nums = [2,7,11,15]
+target = 9
+
+print(two_sum(nums,target))
