1de459641d9bc83b42630a301c587e89cf01b88d
bharathibk <bharathikalai.bk@gmail.com>
2024-04-17 07:30:57 +0530

    leet code 1st completed

--- day7/main.py
+++ day7/main.py
@@ -1,20 +1,21 @@
-#leet code practice problem 1
+#leet code practice problem   #1st code
 def two_sum(nums,target):
 
     num_indices = {}
 
     for i,num in enumerate(nums):
+        # print(f"{i},{num}")
         complement = target - num
 
         if complement in num_indices:
-            return [num_indices[complement],i]
+            return [num_indices[complement],i] 
         num_indices[num] = i
+        # print("the num of indices",num_indices)
 
     return []
 
 
 
 nums = [2,7,11,15]
-target = 9
-
+target = 26
 print(two_sum(nums,target))
---
+++ day7/test1.py
@@ -0,0 +1,30 @@
+
+
+
+
+
+# x = [1,2,3,4]
+
+# for num,i in enumerate(x):
+#     print("the value of i",i,num)
+
+
+# x = {3:0}
+
+# if 2 in x:
+#     print("yes 2 is in x")
+# else:
+#     print("no")
+
+
+
+z = {100:9}
+
+print(z[100][0])
+
+# y = {11:2}
+
+# x = [y[11],3] 
+
+# print(x,"value of x")
+
