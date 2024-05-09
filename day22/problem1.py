378a0bbe4164dab42f27b79731539a5ee1e12af3
bharathibk <bharathikalai.bk@gmail.com>
2024-05-09 07:14:45 +0530

    day 22 file added

---
+++ day22/problem1.py
@@ -0,0 +1,36 @@
+#Print each subsequence on a new line. There will be  of them. No return value is expected.
+
+# Input Format
+
+# The first line contains a single string, s.
+# The second line contains an integer, k, the length of each substring.
+
+# sample input 
+
+# STDIN       Function
+# -----       --------
+# AABCAAADA   s = 'AABCAAADA'
+# 3           k = 3
+
+
+
+# Sample Output
+
+# AB
+# CA
+# AD
+
+
+
+
+def merge_the_tools(string, k):
+    leng = len(string) // k
+    print("the len",leng)
+
+    chuck = 
+    print("string",a)
+
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
